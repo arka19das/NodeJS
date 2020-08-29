7ce6495d05c0178c8cdcbdd258e31a7050e9afd2
Arka Das <arka19das@gmail.com>
2020-08-29 21:28:49 +0530

    MongoDb example 2

--- node-mongo/operations.js
+++ node-mongo/operations.js
@@ -1,47 +1,37 @@
-//file based node module..incorporated in web server
 const assert = require('assert');
-
 
 exports.insertDocument = (db, document, collection, callback) => {
     const coll = db.collection(collection);
     coll.insert(document, (err, result) => {
         assert.equal(err, null);
-
         console.log("Inserted " + result.result.n +
             " documents into the collection " + collection);
-        console.log(result.ops);
         callback(result);
     });
-
 };
 
 exports.findDocuments = (db, collection, callback) => {
     const coll = db.collection(collection);
     coll.find({}).toArray((err, docs) => {
         assert.equal(err, null);
-
         callback(docs);
     });
-
 };
 
 exports.removeDocument = (db, document, collection, callback) => {
     const coll = db.collection(collection);
-    coll.todeleteOne((err, result) => {
+    coll.deleteOne(document, (err, result) => {
         assert.equal(err, null);
-
-        console.log("Removed the document " + document);
-        calllback(result);
+        console.log("Removed the document ", document);
+        callback(result);
     });
-
 };
 
 exports.updateDocument = (db, document, update, collection, callback) => {
     const coll = db.collection(collection);
     coll.updateOne(document, { $set: update }, null, (err, result) => {
         assert.equal(err, null);
-
-        console.log("Updated the document with " + update);
+        console.log("Updated the document with ", update);
         callback(result);
     });
 };
